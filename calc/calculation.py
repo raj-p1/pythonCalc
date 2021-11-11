c65965f48b1afa9fe986b181616bf1a667786f1e
Raj Patel <rp94@njit.edu>
2021-11-11 18:14:52 -0500

    Added the methods to respective files

--- calc/calculation.py
+++ calc/calculation.py
@@ -1,10 +1,9 @@
-"""This is our calculation base class / Abstract Class"""
+"""Calculation file"""
 class Calculation:
     # pylint: disable=too-few-public-methods
-    #contstructor and it is the first function called when an object of the class is instantiated
     """calc main file"""
     def __init__(self,value_a, value_b):
         #self references the instantiated object of the class
-        #Instance properties being shared with the child classes (addition, subtraction, etc...)
+        #Values shared with other classes
         self.value_a = value_a
         self.value_b = value_b
